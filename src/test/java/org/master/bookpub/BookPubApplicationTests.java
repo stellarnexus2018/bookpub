2945eebeee28942b821569d506200d044bf04976
igoryasha <stellarnexus@gmail.com>
2021-04-23 00:38:44 +0300

    Initial commit

---
+++ src/test/java/org/master/bookpub/BookPubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.master.bookpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
